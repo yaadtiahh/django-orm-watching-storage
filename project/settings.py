d84e9084a3da6d18be3a8bbe16812eb8984fbe3d
Lleanin <Fifashf@yandex.ru>
2024-10-01 23:30:06 +0200

    Вынес настройку ALLOWED_HOSTS в переменную окружения

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ DEBUG = env("DEBUG")
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
