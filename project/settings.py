d26ef3f778c6701db2ef55d9755fc5a550a53493
Lleanin <Fifashf@yandex.ru>
2024-10-02 11:41:35 +0200

    добавил типы данных

--- project/settings.py
+++ project/settings.py
@@ -9,19 +9,19 @@ env.read_env()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD")
+        'HOST': env.str("DB_HOST"),
+        'PORT': env.int("DB_PORT"),
+        'NAME': env.str("DB_NAME"),
+        'USER': env.str("DB_USER"),
+        'PASSWORD': env.str("DB_PASSWORD")
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = env("DEBUG")
+DEBUG = env.bool("DEBUG", default=False)
 
 ROOT_URLCONF = 'project.urls'
 
